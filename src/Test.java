af1dbff39141c7113b3cc5d90e04d5c032912538
Inheritance <rejwoan.mrp@gmail.com>
2024-09-15 11:03:42 +0600

    Main Class

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
         BankAccount account = new BankAccount("123456789", 100.0);
 
         try {
-            account.deposit(50000.0);
+            account.deposit(10.0);
             account.withdraw(200.0);
             account.withdraw(50.0);
             account.deposit(10.0);
@@ -12,6 +12,9 @@ public class Test {
             System.out.println(e.getMessage());
         } catch (InvalidAmountException e) {
             System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("The new balance is : " + account.GetBalance());
         }
     }
 }
