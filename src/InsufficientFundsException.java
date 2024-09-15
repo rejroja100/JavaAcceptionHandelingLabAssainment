f50a3ba51eb8a11d2272b7b69771f58c465aa118
Inheritance <rejwoan.mrp@gmail.com>
2024-09-15 10:35:25 +0600

    Exception

---
+++ src/InsufficientFundsException.java
@@ -0,0 +1,11 @@
+public class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
+
+class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
