db4afa05c97adfc5deab4fd5b173bf97b2b3eb16
Inheritance <rejwoan.mrp@gmail.com>
2024-09-15 10:20:48 +0600

    BankAccount Class

---
+++ src/BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount){
+        balance = balance - amount;
+    }
+
+    public double GetBalance(){
+        return balance;
+    }
+
+}
